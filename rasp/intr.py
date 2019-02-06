7e01d0bffae42b0e8c0897d53c94522cc62a8248
OrHell <43348516+OrHell@users.noreply.github.com>
2019-02-06 17:42:34 +0300

    Add files via upload

--- rasp/intr.py
+++ rasp/intr.py
@@ -1,4 +1,5 @@
 import os
+import requests, bs4
 import zipfile##Импорт архивации
 from kivy.app import App##Импорт киви
 from kivy.uix.button import Button
@@ -10,8 +11,8 @@ from kivy.uix.anchorlayout import AnchorLayout
 from kivy.uix.floatlayout import FloatLayout
 
 Config.set('graphics','resizeble','0');##Изминения чтобы окошко не двигалось
-Config.set('graphics','width','480');##Изминения ширина
-Config.set('graphics','height','720');##Изминение высоты
+Config.set('graphics','width','240');##Изминения ширина
+Config.set('graphics','height','360');##Изминение высоты
 
 
 class BoxApp(App):
@@ -20,8 +21,8 @@ class BoxApp(App):
 
 	def build(self):
 		al = AnchorLayout()
-		bl = BoxLayout(spacing = 15,orientation= 'vertical',padding = 55)
-		self.lbl = Label(text = "",size=(360,480))
+		bl = BoxLayout(orientation= 'vertical')
+		self.lbl = Label(text = "",size_hint =[None,None],size = [250,80],font_size = 13)
 		bl.add_widget(self.lbl)
 		bl.add_widget(Button(text = "Понедельник",halign = "center" ,on_press = self.btn_press_ponedel))
 		bl.add_widget(Button(text = "Вторник",halign = "center" ,on_press = self.btn_press_vtornick))
@@ -33,35 +34,42 @@ class BoxApp(App):
 		return al
 		
 	def btn_press_ponedel(self,instance):
-		p=open(u'C:/Users/Гусев/Desktop/Project/rasp/Понедельник.txt','r')
-		s1=p.read()
-		self.lbl.text =s1
-		p.close()
+		s=requests.get('https://orhell.github.io')
+		b=bs4.BeautifulSoup(s.text, "html.parser")
+		p3=b.select('.pon')
+		poned=p3[0].getText()
+		self.lbl.text = poned
+		
 	def btn_press_vtornick(self,instance):
-		v=open(u'C:/Users/Гусев/Desktop/Project/rasp/Вторник.txt','r')
-		s2=v.read()
-		self.lbl.text = s2
-		v.close()
+		s=requests.get('https://orhell.github.io')
+		b=bs4.BeautifulSoup(s.text, "html.parser")
+		p3=b.select('.vtor')
+		vtor=p3[0].getText()
+		self.lbl.text = vtor
 	def btn_press_sreda(self,instance):
-		sr=open(u'C:/Users/Гусев/Desktop/Project/rasp/Среда.txt','r')
-		s3=sr.read()
-		self.lbl.text =s3
-		sr.close()
+		s=requests.get('https://orhell.github.io')
+		b=bs4.BeautifulSoup(s.text, "html.parser")
+		p3=b.select('.sred')
+		sred=p3[0].getText()
+		self.lbl.text = sred
 	def btn_press_chetverg(self,instance):
-		c=open(u'C:/Users/Гусев/Desktop/Project/rasp/Четверг.txt','r')
-		s4=c.read()
-		self.lbl.text =s4
-		c.close()
+		s=requests.get('https://orhell.github.io')
+		b=bs4.BeautifulSoup(s.text, "html.parser")
+		p3=b.select('.chet')
+		chet=p3[0].getText()
+		self.lbl.text = chet
 	def btn_press_pytnica(self,instance):
-		pt=open(u'C:/Users/Гусев/Desktop/Project/rasp/Пятница.txt','r')
-		s5=pt.read()
-		self.lbl.text =s5
-		pt.close()
+		s=requests.get('https://orhell.github.io')
+		b=bs4.BeautifulSoup(s.text, "html.parser")
+		p3=b.select('.pyat')
+		pyat=p3[0].getText()
+		self.lbl.text = pyat
 	def btn_press_subbota(self,instance):
-		su=open(u'C:/Users/Гусев/Desktop/Project/rasp/Суббота.txt','r')
-		s6=su.read()
-		self.lbl.text =s6
-		su.close()
+		s=requests.get('https://orhell.github.io')
+		b=bs4.BeautifulSoup(s.text, "html.parser")
+		p3=b.select('.subb')
+		subb=p3[0].getText()
+		self.lbl.text = subb
 
 
 
